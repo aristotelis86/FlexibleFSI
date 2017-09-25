11d88fcd80e6f94a649047f0f18081aeae8cb14e
Aristotelis Liakatas <agliak@gmail.com>
2017-09-25 01:47:25 +0100

    Creating python tools to interpret and visualize data.

---
+++ FlexibleFSI/PostProcessTools/readCPointsData.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 01:39:37 2017
+
+@author: aristotelis
+"""
+
+import glob
+
+
+filelist = glob.glob("./cpoints*.txt");
+
+
+#f=open("./energy0.txt", "r")
+
+
+#f1=f.readlines()
+
+#for x in f1:
+#    print(x)
