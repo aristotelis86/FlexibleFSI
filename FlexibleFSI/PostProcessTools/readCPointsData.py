673a2d1edf07b8859d2c958a8765871b4202f670
Aristotelis Liakatas <agliak@gmail.com>
2017-09-25 02:21:22 +0100

    Reading information on all of the control points in the simulation.

--- FlexibleFSI/PostProcessTools/readCPointsData.py
+++ FlexibleFSI/PostProcessTools/readCPointsData.py
@@ -7,11 +7,50 @@ Created on Mon Sep 25 01:39:37 2017
 """
 
 import glob
+import re
+import matplotlib.pyplot as plt
 
 
-filelist = glob.glob("./cpoints*.txt");
+filelist = glob.glob("../info/cpoints*.txt");
+
+N = len(filelist);
 
 
+for i in range(0,1):
+    s = filelist[i];
+    num = re.split('(\d+)',s);
+    
+    fileID = open(s,"r");
+    lines = fileID.readlines();
+    xpos =[];
+    ypos = [];
+    xvel = [];
+    yvel = [];
+    xforce = [];
+    yforce = [];
+    for x in lines:
+        ll = x.split(",");
+        xpos.append(ll[0]);
+        ypos.append(ll[1]);
+        xvel.append(ll[2]);
+        yvel.append(ll[3]);
+        xforce.append(ll[4]);
+        yforce.append(ll[5]);
+    
+    plt.figure(0)
+    plt.plot(xpos)
+    plt.figure(1)
+    plt.plot(ypos)
+    plt.figure(2)
+    plt.plot(xvel)
+    plt.figure(3)
+    plt.plot(yvel)
+    plt.figure(4)
+    plt.plot(xforce)
+    plt.figure(5)
+    plt.plot(yforce)
+
+    
 #f=open("./energy0.txt", "r")
 
 
