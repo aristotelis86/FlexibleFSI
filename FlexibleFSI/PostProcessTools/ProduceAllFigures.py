b41124efc4be43a62d166afcf5808762665a6105
Aristotelis Liakatas <agliak@gmail.com>
2017-10-03 23:54:17 +0100

    Performing some checks and cleaning up the tools.

--- FlexibleFSI/PostProcessTools/ProduceAllFigures.py
+++ FlexibleFSI/PostProcessTools/ProduceAllFigures.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Use this script to produce all the figures available for every sheet and 
-# control point of the simulation.
+# Use this script to produce all the basic figures available for every sheet 
+# and control point of the simulation. (Non-specific analysis)
+# Sequential numbering for the sheets and control points starting from 0 is 
+# assumed.
 
 import glob
-import readSpecificInfo as myLib
+import sheetprocessing as myLib
 import time
 
 start = time.time()
@@ -14,14 +16,15 @@ sheetList = glob.glob("../info/sheet*/");
 NSheets = len(sheetList);
 
 for i in range(0,NSheets):
-    myLib.sheet_plots( i );
+    myLib.energy_plot( i )
+    myLib.length_plot( i )
     
     pointList = glob.glob(sheetList[i]+"cpoints*.txt");
     NPoints = len(pointList);
     
     for j in range(0,NPoints):
-        myLib.point_plots( i, j );
-        myLib.frequency_analysis( i, j );
+        myLib.point_plots( i, j )
+        myLib.frequency_plot( i, j )
         
 
 print("It took", time.time()-start,"s to run.");
